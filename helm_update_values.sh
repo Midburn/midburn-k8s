8da67282f40f9e714dc60782b04d96b4b3866d3b
Ori Hoch <ori@uumpa.com>
2017-12-19 19:34:04 +0200

    finalize external app continuous deployment flow

--- helm_update_values.sh
+++ helm_update_values.sh
@@ -3,13 +3,14 @@
 source connect.sh
 
 if [ "${1}" == "" ]; then
-    echo "Usage: ./helm_update_values.sh <YAML_OVERRIDE_VALUES_JSON> [GIT_COMMIT_MESSAGE] [GIT_REPO_TOKEN] [GIT_REPO_SLUG]"
+    echo "Usage: ./helm_update_values.sh <YAML_OVERRIDE_VALUES_JSON> [GIT_COMMIT_MESSAGE] [GIT_REPO_TOKEN] [GIT_REPO_SLUG] [GIT_REPO_BRANCH]"
 fi
 
 YAML_OVERRIDE_VALUES_JSON="${1}"
 GIT_COMMIT_MESSAGE="${2}"
 GIT_REPO_TOKEN="${3}"
 GIT_REPO_SLUG="${4}"
+GIT_REPO_BRANCH="${5:-master}"
 
 if [ "${YAML_OVERRIDE_VALUES_JSON:0:1}" != '{' ]; then
     ! YAML_OVERRIDE_VALUES_JSON=`echo "${YAML_OVERRIDE_VALUES_JSON}" | base64 -d` \
@@ -32,7 +33,7 @@ if [ "${GIT_COMMIT_MESSAGE}" != "" ] && [ "${GIT_REPO_TOKEN}" != "" ] && [ "${GI
         git config user.name "${K8S_ENVIRONMENT_NAME}-deployment-bot"
     ) && echo "failed to git config" && exit 1
 
-    ! git clone --depth 1 "https://github.com/${GIT_REPO_SLUG}.git" "${TEMPDIR}" \
+    ! git clone --depth 1 --branch "${GIT_REPO_BRANCH}" "https://github.com/${GIT_REPO_SLUG}.git" "${TEMPDIR}" \
       && echo "failed git clone" && exit 1
 
     if ! git diff --shortstat --exit-code "environments/${K8S_ENVIRONMENT_NAME}/values.auto-updated.yaml"; then
@@ -40,7 +41,7 @@ if [ "${GIT_COMMIT_MESSAGE}" != "" ] && [ "${GIT_REPO_TOKEN}" != "" ] && [ "${GI
         ! (
             git add "environments/${K8S_ENVIRONMENT_NAME}/values.auto-updated.yaml" &&\
             git commit -m "${GIT_COMMIT_MESSAGE}" &&\
-            git push https://${GIT_REPO_TOKEN}'@'github.com/${GIT_REPO_SLUG}.git master
+            git push https://${GIT_REPO_TOKEN}'@'github.com/${GIT_REPO_SLUG}.git "${GIT_REPO_BRANCH}"
         ) && echo "failed to push changes" && exit 1
     else
         echo "No changes, skipping commit / push"
