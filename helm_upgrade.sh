9917c944217ad329d053262317bec544cedb1f28
Ori Hoch <ori@uumpa.com>
2018-01-17 13:20:58 +0200

    add bi external chart

--- helm_upgrade.sh
+++ helm_upgrade.sh
@@ -6,4 +6,8 @@ DEFAULT_VALUES_FILE=`[ -f values.yaml ] && echo "-f values.yaml"`
 ENVIRONMENT_VALUES_FILE=`[ -f "environments/${K8S_ENVIRONMENT_NAME}/values.yaml" ] && echo "-f environments/${K8S_ENVIRONMENT_NAME}/values.yaml"`
 AUTO_UPDATED_VALUES_FILE=`[ -f "environments/${K8S_ENVIRONMENT_NAME}/values.auto-updated.yaml" ] && echo "-f environments/${K8S_ENVIRONMENT_NAME}/values.auto-updated.yaml"`
 
-helm upgrade $DEFAULT_VALUES_FILE $ENVIRONMENT_VALUES_FILE $AUTO_UPDATED_VALUES_FILE "${K8S_HELM_RELEASE_NAME}-${K8S_ENVIRONMENT_NAME}" . "$@"
+if [ `./read_yaml.py "environments/${K8S_ENVIRONMENT_NAME}/values.yaml" global enableRootChart` == "true" ]; then
+    helm upgrade $DEFAULT_VALUES_FILE $ENVIRONMENT_VALUES_FILE $AUTO_UPDATED_VALUES_FILE "${K8S_HELM_RELEASE_NAME}-${K8S_ENVIRONMENT_NAME}" . "$@"
+else
+    echo "root chart is disabled, skipping helm upgrade"
+fi
