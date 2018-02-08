61955f92b4fb03ad40967d35ac928c14351e3b6a
Ori Hoch <ori@uumpa.com>
2018-02-08 18:26:02 +0200

    finalize volunteers staging+production persistent storage --no-deploy

---
+++ environments/production/volunteersdb_persistent_storage_migration.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+source switch_environment.sh production
+
+VOLUNTEERSDB_POD=`kubectl get pods | grep volunteersdb- | cut -d" " -f1 -`
+[ -z "${VOLUNTEERSDB_POD}" ] && echo "failed to get volunteersdb pod name" && exit 1
+
+echo "deleting existing volunteersdb data from persistent storage (if there is any)"
+read -p "press <Enter> to continue"
+! gcloud compute ssh midburn-k8s-persistent-storage-vm --command="bash -c 'sudo rm -rf /data/${K8S_ENVIRONMENT_NAME}/volunteersdb'" && echo failed && exit 1
+
+echo "deleting volunteersdb service for a clean migration"
+read -p "press <Enter> to continue"
+! kubectl delete service volunteersdb && echo failed && exit 1
+
+echo "creating storage and importing data from pod"
+gcloud compute ssh midburn-k8s-persistent-storage-vm --command="bash -c '
+    mkdir -p /data/${K8S_ENVIRONMENT_NAME}
+    ! mkdir /data/${K8S_ENVIRONMENT_NAME}/volunteersdb && echo failed to create volunteersdb dir && exit 1
+    cd
+    ! [ -e midburn-k8s ] && git clone https://github.com/Midburn/midburn-k8s.git
+    cd midburn-k8s
+    ! (git checkout master && git pull origin master) && echo failed && exit 1
+    source switch_environment.sh ${K8S_ENVIRONMENT_NAME}
+    echo copying from pod ${VOLUNTEERSDB_POD}
+    ! kubectl cp ${VOLUNTEERSDB_POD}:/data/db /data/${K8S_ENVIRONMENT_NAME}/volunteersdb && echo failed && exit 1
+    echo setting permissions
+    sudo chown -R root:root /data/${K8S_ENVIRONMENT_NAME}/volunteersdb
+    echo great success
+    exit 0
+'"
+
+echo "redeploying volunteers chart to enable the service via the new storage setup"
+./helm_upgrade_external_chart.sh volunteers
